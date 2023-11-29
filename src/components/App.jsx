5c4db23eb269923e86bc192e7f2c12e1890da9cd
Ostap <Ostapkriven13@gmail.com>
2023-11-29 21:56:26 +0200

    і
    і

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,15 @@ function App() {
 
   return (
       <div>
+        {/* <div className='container'>
+    <form action="#">
+      <select name="language" className='select'>
+          <option value="en" className='en'>En</option>
+        <option value="uk"className='uk'>Uk</option>
+      </select>
+    </form>
+    </div> */}
+
       <div className='container'>
         <LanguageSwitcher />
         </div>
