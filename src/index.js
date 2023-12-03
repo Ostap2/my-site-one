a31da1071156696db29beab54b2647bde706e788
Ostap <Ostapkriven13@gmail.com>
2023-12-03 11:47:44 +0200

    s
    s

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import App from './components/App';
 import { createRoot } from 'react-dom/client';
-import Text from './i18n';
-import { I18nextProvider } from 'react-i18next';
+
 
 import 'index.css'
 
 const rootElement = document.getElementById('root');
 const reactRoot = createRoot(rootElement);
 
-reactRoot.render(<I18nextProvider i18n={Text}><App /></I18nextProvider>);
+reactRoot.render(<App />);
