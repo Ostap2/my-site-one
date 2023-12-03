e2f5eea0941c88927f043117172e766dcf2f2a0d
Ostap <Ostapkriven13@gmail.com>
2023-12-03 11:24:25 +0200

    s
    s

--- src/components/Test/Test.jsx
+++ src/components/Test/Test.jsx
@@ -8,7 +8,17 @@ function Test() {
     <div>
         <h1>{t('common.title')}</h1>
         <p>{t('common.paragraf')}</p>
-        <button>{t('common.button')}</button>
+
+
+
+
+
+
+
+
+
+
+
     </div>
   );
 }
